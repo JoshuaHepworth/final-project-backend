482548e75959a99521e3052f197539d23bda7d9d
Josh Hepworth <jrhepworth12@gmail.com>
2018-11-27 16:37:43 -0600

    added migrations tables and models

---
+++ db/migrations.sql
@@ -0,0 +1,29 @@
+DROP DATABASE IF EXISTS news_app;
+CREATE DATABASE news_app;
+
+\c news_app
+
+CREATE TABLE users(
+	id SERIAL PRIMARY KEY,
+	username VARCHAR(64),
+	password_digest VARCHAR(60)
+);
+
+CREATE TABLE articles(
+	id SERIAL PRIMARY KEY,
+	source VARCHAR(64),
+	author VARCHAR(64),
+	title VARCHAR(64),
+	description VARCHAR(260),
+	imgUrl VARCHAR(512),
+	apiId VARCHAR(64),
+	articleUrl VARCHAR(512),
+	user_id INTEGER REFERENCES users(id)
+);
+
+CREATE TABLE comments(
+	id SERIAL PRIMARY KEY,
+	text TEXT,
+	user_id INTEGER REFERENCES users(id),
+	article_id INTEGER REFERENCES articles(id)
+);
